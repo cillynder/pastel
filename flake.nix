b30c8d4558f1affe4f2dbca6e07f066544c3c24a
Cilly Leang <me@lava.moe>
2026-02-03 17:59:17 +1100

    chore: allow nix building on other system arches

--- flake.nix
+++ flake.nix
@@ -2,19 +2,18 @@
   description = "weebsite";
 
   inputs = {
+    flake-utils.url = "github:numtide/flake-utils";
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
     pnpm2nix.url = "github:wrvsrx/pnpm2nix-nzbr/adapt-to-v9";
     pnpm2nix.inputs.nixpkgs.follows = "nixpkgs";
   };
 
-  outputs = { pnpm2nix, ... }:
-  let
-    system = "x86_64-linux";
-  in {
-    packages.${system}.default = pnpm2nix.packages.${system}.mkPnpmPackage {
-      name = "pastel";
-      src = ./.;
-      installInPlace = true;
-    };
-  };
+  outputs = { pnpm2nix, flake-utils ,... }:
+    flake-utils.lib.eachDefaultSystem(system: {
+      defaultPackage = pnpm2nix.packages.${system}.mkPnpmPackage {
+        name = "pastel";
+        src = ./.;
+        installInPlace = true;
+      };
+    });
 }
